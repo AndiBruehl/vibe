ac4bae7762a0d3926c1b3c5ca1a1f8d4dad602ec
AndiBruehl <a.bruehl2019@gmail.com>
2026-03-12 20:53:13 +0100

    adding and styling navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,117 +1,71 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
 import {
+  CameraIcon,
   HomeIcon,
-  SearchIcon,
-  PlusSquareIcon,
   LayoutGridIcon,
+  SearchIcon,
   UserIcon,
 } from "lucide-react";
+import Link from "next/link";
 
-export default function Navbar() {
-  const pathname = usePathname();
-
-  const linkBase =
-    "group flex min-w-[56px] flex-col items-center no-underline transition-colors duration-200";
-
-  const iconBase =
-    "size-7 transition-transform duration-200 group-hover:scale-90";
-
-  const textBase =
-    "mt-1 text-[11px] font-medium tracking-wide opacity-0 transition-opacity duration-200 group-hover:opacity-100";
-
-  const gradient =
-    "size-16 flex items-center justify-center rounded-full bg-gradient-to-tr from-[var(--ig-orange)] to-[var(--ig-red)]";
-
-  const gradientText =
-    "bg-gradient-to-tr from-[var(--ig-orange)] to-[var(--ig-red)] bg-clip-text text-transparent";
-
-  const activeClass = "text-[var(--ig-red)]";
-  const inactiveClass = "text-black hover:text-slate-700";
-
+export default function MobileNav() {
   return (
-    <nav className="fixed bottom-0 left-0 right-0 border-t border-slate-200 bg-white/70 backdrop-blur-xl shadow-[0_-4px_20px_rgba(0,0,0,0.06 )]">
-      <div className="mx-auto flex max-w-md items-center justify-center gap-10 px-6 py-2">
-        <Link
-          href="/"
-          className={`${linkBase} ${
-            pathname === "/" ? activeClass : inactiveClass
-          }`}
-        >
-          <HomeIcon className={iconBase} />
-          <span
-            className={`${textBase} ${pathname === "/" ? "opacity-100" : ""}`}
-          >
-            HOME
-          </span>
-        </Link>
+    <div className="block md:hidden fixed bottom-0 left-0 right-0">
+      <div className="flex text-gray-700 dark:text-gray-300 *:flex *:items-center">
+        <div className="pl-2 bg-white dark:bg-gray-700 rounded-t-xl w-full relative z-10 *:size-12 *:flex *:flex-col *:items-center *:justify-center justify-around pt-4">
+          <Link href="/" className="group">
+            <HomeIcon className="transition-transform duration-200 group-hover:scale-90" />
+            <span className="text-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              HOME
+            </span>
+          </Link>
 
-        <Link
-          href="/search"
-          className={`${linkBase} ${
-            pathname.startsWith("/search") ? activeClass : inactiveClass
-          }`}
-        >
-          <SearchIcon className={iconBase} />
-          <span
-            className={`${textBase} ${
-              pathname.startsWith("/search") ? "opacity-100" : ""
-            }`}
-          >
-            SEARCH
-          </span>
-        </Link>
+          <Link href="/search" className="group">
+            <SearchIcon className="transition-transform duration-200 group-hover:scale-90" />
+            <span className="text-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              SEARCH
+            </span>
+          </Link>
+        </div>
 
-        {/* CREATE */}
-
-        <Link href="/create" className={linkBase}>
-          <div className={`${gradient} p-0.5`}>
-            <PlusSquareIcon className="size-7 text-white transition-transform duration-200 group-hover:scale-90" />
+        <div className="size-14 relative -top-4 justify-center w-[140px]">
+          <div className="absolute bg-blue-500 bg-clip-text border-white dark:border-gray-700 border-t-transparent dark:border-t-transparent border-l-transparent dark:border-l-transparent border-[50px] rounded-full rotate-45">
+            <div className="border-4 size-15 border-transparent">
+              <Link
+                href="/create"
+                className="group -rotate-45 bg-gradient-to-tr from-ig-orange to-ig-red to-70% size-12 flex flex-col items-center justify-center text-white rounded-full"
+              >
+                <div className="flex size-14 items-center justify-center rounded-full bg-gradient-to-tr from-(--ig-orange) to-(--ig-red) shadow-md transition-transform duration-200 group-hover:scale-105">
+                  <CameraIcon className="mt-25 size-10 text-white transition-transform duration-200 group-hover:scale-90  mb-25" />
+                </div>{" "}
+                <span className="mt-3 translate-y-3 text-[13px] font-extrabold tracking-wide opacity-0 group-hover:opacity-100 transition-all duration-200 bg-gradient-to-tr from-[var(--ig-orange)] to-[var(--ig-red)] bg-clip-text text-transparent drop-shadow-[0_1px_1px_rgba(0,0,0,0.18)]">
+                  CREATE
+                </span>
+              </Link>
+            </div>
           </div>
+        </div>
 
-          <span
-            className={`${textBase} ${gradientText} ${
-              pathname.startsWith("/create") ? "opacity-100" : ""
-            }`}
+        <div className="pr-2 w-full bg-white dark:bg-gray-700 rounded-t-xl relative z-10 *:size-12 *:flex *:flex-col *:items-center *:justify-center justify-around pt-4">
+          <Link
+            href="/browse"
+            className="group text-ig-red dark:text-ig-orange"
           >
-            CREATE
-          </span>
-        </Link>
+            <LayoutGridIcon className="transition-transform duration-200 group-hover:scale-90" />
+            <span className="text-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              BROWSE
+            </span>
+          </Link>
 
-        <Link
-          href="/browse"
-          className={`${linkBase} ${
-            pathname.startsWith("/browse") ? activeClass : inactiveClass
-          }`}
-        >
-          <LayoutGridIcon className={iconBase} />
-          <span
-            className={`${textBase} ${
-              pathname.startsWith("/browse") ? "opacity-100" : ""
-            }`}
-          >
-            BROWSE
-          </span>
-        </Link>
-
-        <Link
-          href="/profile"
-          className={`${linkBase} ${
-            pathname.startsWith("/profile") ? activeClass : inactiveClass
-          }`}
-        >
-          <UserIcon className={iconBase} />
-          <span
-            className={`${textBase} ${
-              pathname.startsWith("/profile") ? "opacity-100" : ""
-            }`}
-          >
-            PROFILE
-          </span>
-        </Link>
+          <Link href="/profile" className="group">
+            <UserIcon className="transition-transform duration-200 group-hover:scale-90" />
+            <span className="text-[10px] opacity-0 group-hover:opacity-100 transition-opacity duration-200">
+              PROFILE
+            </span>
+          </Link>
+        </div>
       </div>
-    </nav>
+    </div>
   );
 }
