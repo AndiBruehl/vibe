86c550c256f2764e3a08b4827f4e98521d1de828
AndiBruehl <a.bruehl2019@gmail.com>
2026-09-10 10:05:04 +0200

    fix: show 0.1.42.1 in web settings

--- src/app/components/AppVersion.tsx
+++ src/app/components/AppVersion.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-const WEB_VERSION = "0.1.42";
+const WEB_VERSION = "0.1.42.1";
 
 export default function AppVersion() {
   const [desktopVersion, setDesktopVersion] = useState<string | null>(null);
