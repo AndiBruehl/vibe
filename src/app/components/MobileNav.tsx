30c3045e8ab774c3b58bfb16a53340f5965af6d7
AndiBruehl <a.bruehl2019@gmail.com>
2026-09-15 16:24:30 +0200

    fix: match mobile create button surround to theme

--- src/app/components/MobileNav.tsx
+++ src/app/components/MobileNav.tsx
@@ -57,8 +57,8 @@ export default function MobileNav({
           </Link>
         </div>
 
-        <div className="size-14 relative -top-4 justify-center w-35">
-          <div className="absolute bg-red-500 bg-clip-text border-white dark:border-gray-800 border-t-transparent dark:border-t-transparent border-l-transparent dark:border-l-transparent border-50 rounded-full rotate-45">
+        <div className="relative -top-4 size-14 shrink-0 justify-center">
+          <div className="absolute rounded-full border-[var(--ig-surface-bg)] bg-red-500 bg-clip-text border-t-transparent border-l-transparent dark:border-gray-800 dark:border-t-transparent dark:border-l-transparent rotate-45">
             <div className="border-4 size-15 border-transparent">
               <Link
                 href="/create"
