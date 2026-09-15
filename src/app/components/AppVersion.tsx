31dc03e9d6cefabb968cf3546b0a2b0ff6cabe3f
AndiBruehl <a.bruehl2019@gmail.com>
2026-09-16 00:13:33 +0200

    chore: release beta 0.1.69.4

--- src/app/components/AppVersion.tsx
+++ src/app/components/AppVersion.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useState } from "react";
 import useVibeLanguage from "@/app/components/useVibeLanguage";
 
-const WEB_VERSION = "0.1.69.3.1";
+const WEB_VERSION = "0.1.69.4";
 const BETA_FLAIR = " 😈🔥";
 
 export default function AppVersion() {
